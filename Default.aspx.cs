8384a27db7caae399f6496c492b8ee222d43bc54
dinocizmic <dinocizmic@me.com>
2016-02-18 19:13:16 -0800

    Added files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Linq;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataStudentsDataContext studentdb = new DataStudentsDataContext();
+        var allstudents = from student in studentdb.DataStudents
+                          where NewStudents.StudentID > 1
+                          select NewStudents;
+        GridView1.DataSource = allstudents;
+        GridView1.DataBind();
+    }
+}
