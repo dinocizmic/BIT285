33cf5a8b08ce9b988f0c416746c8b6523b79484e
dinocizmic <dinocizmic@me.com>
2016-03-14 17:31:00 -0700

    Added files via upload
    I think that this is for SQL Server LINQ, I know that I had issues with SQL the first day so I dont know if this is 100% correct.

---
+++ bit285-linq/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[StudentID] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(50) NULL, 
+    [LastName] NCHAR(50) NULL, 
+    [City] NCHAR(50) NULL, 
+    [State] NCHAR(2) NULL, 
+    [Country] NCHAR(50) NULL, 
+    [Zip] NCHAR(50) NULL, 
+    [Phone] NCHAR(50) NULL
+)
